fdc8cd2a780ccd0e5e8189a76e6ce2be1c91ab0c
NguyenToan <112460683+Togban@users.noreply.github.com>
2023-04-12 14:13:59 +0700

    Update readme.py

--- Fantan/readme.py
+++ Fantan/readme.py
@@ -1,8 +1,8 @@
 
-Giá trị bài: k % 12
+Giá trị bài: k % 13
 Giá trị bài bắt đầu từ 0 -> 51
 0 là 1, 1 là 2 ...
-Loại bài: k // 12
+Loại bài: k // 13
 0,1,2,3: Cơ, rô, chuồn, bích
 Player_state
 [0:52]: Player card
